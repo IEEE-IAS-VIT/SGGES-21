168751baeada9fbcff66b9f376f68cb027ac70f4
Hritam Dutta <hritamloyola10@gmail.com>
2021-07-03 14:14:20 +0530

    Final Site

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# SGGES-21
+<img src="https://github.com/ShubhMittal44/SGGES-21/blob/main/assets/SGGES%20Logo.png" alt= "Conference Logo" width="250" height="60"/>
+
+## Technically Sponsored by IEEE IAS VIT.
+
+> Virtual International Conference on Smart Grid and Green Energy Systems (SGGES’21) will be a platform for the researchers, academicians as well as professionals from all over the world to present, discuss and promote advances in research and practice in the field of smart grid and renewable energy systems.
+
+#### Made using HTML5, CSS3, Bootstrap and JavaScript.
+
+**_Website Link_** : [SGGES-21](https://shubhmittal44.github.io/SGGES-21/Home_page/index.html)
---
+++ committee/committee.js
@@ -0,0 +1,20 @@
+document.getElementById("b1").addEventListener("click",myFunction);
+function myFunction()
+{
+    document.getElementById("pop").className="main4 shadow";
+    document.querySelector("#pop").innerHTML="<p><b>Dr. Chitra A.</b><br> Associate professor<br>Department of Energy & Power Electronics School of Electrical Engineering<br>VIT Vellore, India<br> E-Mail: chitra.a@vit.ac.in <br>Mobile:+91 9894760447 </p><br>";
+}
+document.getElementById("b2").addEventListener("click",myFunction1);
+function myFunction1()
+{
+    document.getElementById("pop").className="main4 shadow";
+    document.querySelector("#pop").innerHTML="<p><b>Dr. V. Indragandhi </b><br> Associate professor<br>Department of Energy & Power Electronics School of Electrical Engineering<br>VIT Vellore, India<br> E-Mail: indragandhi.v@vit.ac.in <br>Mobile:+91 97506 03539 </p><br>";
+}
+document.getElementById("b3").addEventListener("click",myFunction2);
+function myFunction2()
+{
+    document.getElementById("pop").className="main4 shadow";
+    document.querySelector("#pop").innerHTML="<p><b>Dr. Razia Sultana W.</b><br> Associate professor<br>Department of Energy & Power Electronics School of Electrical Engineering<br>VIT Vellore, India<br> E-Mail: wraziasultana@vit.ac.in <br>Mobile:+91 9943295041</p><br>";
+}
+
+  
