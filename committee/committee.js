eb6eff0de15ab0f4e6646dd3c622b9cf73e16d4d
Gunpreet Singh <62554005+sgunpreetsandhu01@users.noreply.github.com>
2021-07-03 21:29:52 +0530

    Update committee.js

--- committee/committee.js
+++ committee/committee.js
@@ -16,4 +16,17 @@ function myFunction2()
     document.getElementById("pop").className="main4 shadow";
     document.querySelector("#pop").innerHTML="<p><b>Dr. Razia Sultana W.</b><br> Associate professor<br>Department of Energy & Power Electronics School of Electrical Engineering<br>VIT Vellore, India<br> E-Mail: wraziasultana@vit.ac.in <br>Mobile:+91 9943295041</p><br>";
 }
+document.getElementById("b4").addEventListener("click",myFunction3);
+function myFunction3()
+{
+    document.getElementById("organizers").className="main4 shadow";
+    document.querySelector("#organizers").innerHTML="<p>Aryan Agrawal<br>Ananya Jaiswal<br>Shantanu Nikam<br>Palak Toshniwal<br>Naveen Ravishankar<br>Rohith Gamini<br>Lovish Sethi<br>Anshul Tripathi<br>Palash Sinha<br>Aaradhya</p>";
+
+}
+document.getElementById("b5").addEventListener("click",myFunction4);
+function myFunction4()
+{
+    document.getElementById("organizers").className="main4 shadow";
+    document.querySelector("#organizers").innerHTML="<p>Nupoor Raj<br>Fatema Malik<br>Yash Agarwal<br>Davya Vuyyuru<br>Amisha Rathore<br>Raunak Shukla<br>Harshita Suresh<br>Hem Prasath<br>Jaydev Jangiti<br>Tanay Shukla<br></p>";
+}
 
